26568a0e20c5196707aa1409023d1f5dda4c8f1d
kumar44424 <45359851+kumar44424@users.noreply.github.com>
2020-01-23 11:35:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -490,19 +490,6 @@ resource "aws_instance" "kali" {
 <powershell>
 net user ${var.cam_user} '${var.cam_pwd}' /add /y
 net localgroup administrators ${var.cam_user} /add
-
-winrm quickconfig -q
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="300"}'
-winrm set winrm/config '@{MaxTimeoutms="1800000"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
-
-netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localport=5985 action=allow
-netsh advfirewall firewall add rule name="WinRM 5986" protocol=TCP dir=in localport=5986 action=allow
-
-net stop winrm
-sc.exe config winrm start=auto
-net start winrm
 </powershell>
 EOF
 
